edbe9280599aebead36898998e45f9adb6891253
Davide Tonelli <davtonell@gmail.com>
2023-10-21 14:11:58 +0200

    finally ported howclose to lua
    also removed autocommand for neotree because causes problems
    while closing buffers

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,11 +1,11 @@
 require "helpers/globals"
 
-api.nvim_create_autocmd("VimEnter", {
-    -- very ugly but it works
-    callback = function ()
-        cmd("set nornu nonu")
-        cmd("Neotree toggle show")
-        cmd("wincmd p")
-        cmd("set rnu nu")
-    end
-})
+-- api.nvim_create_autocmd("VimEnter", {
+--     -- very ugly but it works
+--     callback = function ()
+--         cmd("set nornu nonu")
+--         cmd("Neotree toggle show")
+--         cmd("wincmd p")
+--         cmd("set rnu nu")
+--     end
+-- })
---
+++ lua/out.lua
@@ -0,0 +1,16 @@
+{
+  bufnr = 2,
+  changed = 0,
+  changedtick = 1,
+  hidden = 1,
+  lastused = 0,
+  linecount = 1,
+  listed = 0,
+  lnum = 0,
+  loaded = 1,
+  name = "",
+  variables = {
+    changedtick = 1
+  },
+  windows = {}
+}
