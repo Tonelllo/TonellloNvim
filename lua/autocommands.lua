b3eba7c9cc97d69f0281c288e06a04c0b18a630b
Davide Tonelli <davtonell@gmail.com>
2026-03-22 23:37:24 +0100

    Added ripgrep install if not present

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -59,41 +59,49 @@ api.nvim_create_autocmd({ "BufEnter", "BufLeave" }, {
     end
 })
 
+function installProgram(name, cmd)
+    local ret = vim.fn.confirm("Devpod not installed. Install?", "&Yes\n&No", 2)
+    if ret == 1 then
+        vim.notify("Installing " .. name)
+        vim.cmd("split")
+        vim.cmd("resize 15")
+        local buf = vim.api.nvim_create_buf(false, true) -- [listed=false, scratch=true]
+
+        vim.api.nvim_set_current_buf(buf)
+
+        vim.fn.termopen(
+            cmd,
+            {
+                on_exit = function(_, code)
+                    if code == 0 then
+                        vim.schedule(function()
+                            vim.api.nvim_buf_delete(buf, { force = true })
+                        end)
+                    else
+                        vim.defer_fn(function()
+                            vim.schedule(function()
+                                vim.notify("Command failed (exit code " .. code .. ")", vim.log.levels.ERROR)
+                                vim.api.nvim_buf_delete(buf, { force = true })
+                            end)
+                        end, 1500)
+                    end
+                end
+            })
+        vim.cmd("startinsert")
+    end
+end
+
 api.nvim_create_autocmd({ "VimEnter" }, {
     pattern = "VeryLazy",
     callback = function()
         local remoteInstalled = require('lazy.core.config').plugins["remote-nvim.nvim"] ~= nil
         local devpodInstalled = vim.fn.executable("devpod") == 1
         if remoteInstalled and not devpodInstalled then
-            local ret = vim.fn.confirm("Devpod not installed. Install?", "&Yes\n&No", 2)
-            if ret == 1 then
-                vim.notify("Installing devpod")
-                vim.cmd("split")
-                vim.cmd("resize 15")
-                local buf = vim.api.nvim_create_buf(false, true) -- [listed=false, scratch=true]
-
-                vim.api.nvim_set_current_buf(buf)
-
-                vim.fn.termopen(
-                    [[curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod]],
-                    {
-                        on_exit = function(_, code)
-                            if code == 0 then
-                                vim.schedule(function()
-                                    vim.api.nvim_buf_delete(buf, { force = true })
-                                end)
-                            else
-                                vim.defer_fn(function()
-                                    vim.schedule(function()
-                                        vim.notify("Command failed (exit code " .. code .. ")", vim.log.levels.ERROR)
-                                        vim.api.nvim_buf_delete(buf, { force = true })
-                                    end)
-                                end, 1500)
-                            end
-                        end
-                    })
-                vim.cmd("startinsert")
-            end
+            installProgram("devpod",
+                [[curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod]])
+        end
+        if not vim.fn.executable("ripgrep") then
+            installProgram("ripgrep", [[sudo apt install ripgrep]])
         end
     end
 })
